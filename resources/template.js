0eddfd0bc31da2e6dab47eac6ec57d7d10cfeb1b
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2023-11-04 15:54:27 +0000

    Simplifications

--- resources/template.js
+++ resources/template.js
@@ -2,35 +2,26 @@
 ;(function () {
     'use strict'
 
-    console.log(window['tiTestbed'])
-
-    const log = window['uibuilder'].log
+    const tiTestbed = window['tiTestbed']
+    // const log = tiTestbed.log
 
     /** Module name must match this nodes html file @constant {string} moduleName */
-    const moduleName = 'ti-dummy'
-    /** Node's label @constant {string} paletteCategory */
-    const nodeLabel = moduleName
-    /** Node's palette category @constant {string} paletteCategory */
-    const paletteCategory = window['tiTestbed'].paletteCategory
-    /** Node's background color @constant {string} paletteColor */
-    const paletteColor = 'var(--ti-testbed-node-colour)'
+    const moduleName = 'ti-template'
 
     // Standard width for typed input fields
-    // const tiWidth = window['tiTestbed'].typedInputWidth
+    // const tiWidth = tiTestbed.typedInputWidth
 
     /** Prep for edit
      * @param {*} node A node instance as seen from the Node-RED Editor
      */
     function onEditPrepare(node) {
-        log.log('onEditPrepare', node)
+        // log.log('onEditPrepare', node)
 
         window['tiDoTooltips']('#ti-edit-panel') // Do this at the end
     } // ----- end of onEditPrepare() ----- //
 
     // @ts-ignore
     RED.nodes.registerType(moduleName, {
-        category: paletteCategory,
-        color: paletteColor,
         defaults: {
             name: { value: '' },
             topic: { value: '' },
@@ -41,10 +32,12 @@
         outputs: 1,
         outputLabels: ['Output 1'],
         icon: 'font-awesome/fa-code',
-        paletteLabel: nodeLabel,
         label: function () {
             return this.name || moduleName
         },
+        category: tiTestbed.paletteCategory,
+        color: 'var(--ti-testbed-node-colour)',
+        paletteLabel: moduleName,
 
         /** Prepares the Editor panel */
         oneditprepare: function () { onEditPrepare(this) },
