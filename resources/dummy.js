afd48f24c259cd570cb195c7302adda56a419195
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2023-11-28 18:03:20 +0000

    Add typed inputs for testing. Add input validation tests and table of results.

--- resources/dummy.js
+++ resources/dummy.js
@@ -24,20 +24,96 @@
         console.log('[dummy] ti-testbed:ti-dummy-node-deleted was fired', node)
     })
 
-    /** Validate the topic input - this is called on Editor load as well as on change
+    /** Validate the topic input - this is called on Editor load, panel open, as well as on change
      * @param {*} value The input value to be validated
      * @returns {boolean} True if valid
      */
     function validateTopic(value) {
-        // log('validateTopic function called')
+        /** Notes:
+         *  1) `this` is the node instance configuration as at last press of Done.
+         *  2) Validation fns are run:
+         *      a) On every node instance when the Editor is loaded (e.g. panel not visible) - value is populated but jQ val is undefined.
+         *      b) When a new node instance added - value & jq value both `undefined`.
+         *      c) When the config panel is opened for an instance.
+         *      d) When ANY field value has changed (on keypress) - value & jq value are the same.
+         *      e) When ANY field value has changed (on blur) - value & jq value are the same.
+         *      f) When ANYTHING happens to ANY field (e.g. copy, paste, changing to a different window, ...)
+         *      f) When "Done" is clicked (even if no data changed).
+         *  4) jq values are undefined when this is called on load (e.g. panel not open).
+         *  5) value and jq value both undefined when a new instance added to flow. Validation is fired at that point.
+         *
+         * Because of the number of calls - KEEP THIS SHORT AND SIMPLE
+         * Use jQuery change functions by preference and maybe reserve this for cases where another process
+         * might change the value.
+         *
+         * This table shows how to differentiate different call executions (NB: value 🟰 jQ always)
+         *                          | Visible | this.changed |
+         * On page load             |   ❌   |    ❌        | value !== this.[propName]
+         * On panel open            |   ✔️   |    ❌/✔️     | value 🟰 this.[propName]
+         * On other field change    |   ✔️   |    ❌/✔️     | value 🟰 this.[propName]
+         * On other panel trigger   |   ✔️   |    ❌/✔️     | value 🟰 this.[propName]
+         * On propName field change |   ✔️   |    ❌/✔️     | value !== this.[propName]
+         * On "Done"                |   ✔️   |    ❌/✔️     | value 🟰 this.[propName]
+         */
+
+        // As this is called on Editor load, when panel opened AND when a change made
+        // you may want to do things only when the panel is open (visible)
+        if ($('#node-input-topic').is(':visible')) {
+            console.log('[ti-dummy] #node-input-topic IS VISIBLE. ', `Changed? '${this.changed}'.`, `Value: '${value}'.`, `this.topic: '${this.topic}'.`, `jQ value: ${$('#node-input-topic').val()}.`)
+        } else {
+            console.log('[ti-dummy] #node-input-topic IS NOT VISIBLE. ', `Changed? '${this.changed}'.`, `Value: '${value}'.`, `this.topic: '${this.topic}'.`, `jQ value: ${$('#node-input-topic').val()}.`)
+        }
+
         return true
     }
+
+    const x = [
+        'msg', 'flow', 'global',
+        'str', 'env', 'jsonata', 're',
+    ]
 
     /** Prep for edit
      * @param {*} node A node instance as seen from the Node-RED Editor
      */
     function onEditPrepare(node) {
         // log.log('onEditPrepare', node)
+
+        // REF: https://nodered.org/docs/api/ui/typedInput/, 
+        $('#node-input-tyiMsg')
+            .typedInput({
+                types: ['msg', 'str'],
+                default: 'msg',
+                typeField: $('#node-input-tyiMsgSourceType'),
+            }).typedInput('width', tiTestbed.typedInputWidth)
+
+        $('#node-input-tyiGlobal')
+            .typedInput({
+                types: ['global', 'str'],
+                default: 'global',
+                typeField: $('#node-input-tyiGlobalSourceType'),
+            }).typedInput('width', tiTestbed.typedInputWidth)
+
+        $('#node-input-tyiEnv')
+            .typedInput({
+                types: ['env', 'str'],
+                default: 'env',
+                typeField: $('#node-input-tyiEnvSourceType'),
+            }).typedInput('width', tiTestbed.typedInputWidth)
+
+        $('#node-input-tyiExpr')
+            .typedInput({
+                types: ['jsonata', 'str'],
+                default: 'jsonata',
+                typeField: $('#node-input-tyiExprSourceType'),
+            }).typedInput('width', tiTestbed.typedInputWidth)
+
+
+        $('#node-input-tyiNode')
+            .typedInput({
+                types: ['node', 'str'],
+                default: 'node',
+                typeField: $('#node-input-tyiNodeSourceType'),
+            }).typedInput('width', tiTestbed.typedInputWidth)
 
         tiTestbed.doTooltips('#ti-edit-panel') // Do this at the end
     } // ----- end of onEditPrepare() ----- //
@@ -48,6 +124,16 @@
         defaults: {
             name: { value: '' },
             topic: { value: '', validate: validateTopic },
+            tyiMsg: { value: '' },
+            tyiMsgSourceType: { value: '' },
+            tyiGlobal: { value: '' },
+            tyiGlobalSourceType: { value: '' },
+            tyiEnv: { value: '' },
+            tyiEnvSourceType: { value: '' },
+            tyiExpr: { value: '' },
+            tyiExprSourceType: { value: '' },
+            tyiNode: { value: '' },
+            tyiNodeSourceType: { value: '' },
         },
         align: 'left',
         inputs: 1,
